2a01a155c2c2274bd1e0092ab33b3b71f02e89ec
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2023-12-07 00:18:36 +0000

    CICD: Added datetime code

--- transform.py
+++ transform.py
@@ -1,2 +1,25 @@
 import pandas as pd
+from datetime import datetime
+from pytz import timezone
 
+central_tz = timezone('US/Central')
+date_frmt = '%Y%m%d_%H%M'
+my_date = datetime.now(central_tz).strftime(date_frmt)
+print(my_date)
+
+def extract() -> None:
+    return None
+
+def transf() -> None:
+    return None
+
+def load() -> None:
+    return None
+
+if __name__ == "__main__":
+    source_file_name = ""
+    # file name format = supplier_name_suppinv_YYYYMMDD_HHmm
+    desti_file_name = f""
+    extract()
+    transf()
+    load()
