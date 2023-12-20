c1c14d0a593bfb44b881265221c4de834759441c
sanya pc_beast <sanyashireen@gmail.com>
2023-12-20 13:06:42 -0600

    CICD:Removed local writes

--- transform.py
+++ transform.py
@@ -32,7 +32,7 @@ date_frmt = '%Y%m%d_%H%M'
 my_date = datetime.now(central_tz).strftime(date_frmt)
 print(my_date)
 
-def connect_load(df_i, df_p, desti_file_i, desti_file_p):
+def trans_conn_load(df_i, df_p, desti_file_i, desti_file_p):
     # Creating list of column names to preserve the order of the columns
     cols_i = ['FileCreationDate', 
                 'FileCreationTime', 
@@ -150,110 +150,6 @@ def extract_gdrive(url, sheets):
 
     return df_i, df_p
 
-
-
-def extract_local(data_dir, source_file, sheets):
-    file_loc = os.path.join(data_dir, source_file)
-    df_i = pd.read_excel(file_loc, sheet_name = sheets[0])
-    df_p = pd.read_excel(file_loc, sheet_name = sheets[1])
-    
-    print(f"Extracted Inventory data of size (rows,cols): {df_i.shape}")
-    print(f"Extracted Production data of size (rows,cols): {df_p.shape}")
-    
-    # testing
-    #print(df_i.head())
-    #print(df_p.head())
-    #print(df_i.columns)
-    #print(df_p.columns)
-    #print(df_i.dtypes)
-
-    return df_i, df_p
-
-def transf_write_local(df_i, df_p, desti_file_i, desti_file_p) -> None:
-    # Creating list of column names to preserve the order of the columns
-    cols_i = ['FileCreationDate', 
-                'FileCreationTime', 
-                'Brand', 
-                'LocationID', 
-                'LocationName', 
-                'InventoryDate', 
-                'InventoryTime', 
-                'DCItemNumber', 
-                'ExclusiveIndicator', 
-                'CatchWeightIndicator', 
-                'ItemName', 
-                'ItemDescription', 
-                'ItemPriceperUOM', 
-                'ItemPriceUOM', 
-                'BrandItemNumber', 
-                'ItemCasePackQuantity', 
-                'ItemCasePackQuantityUOM', 
-                'ManufacturerItemNumber', 
-                'ManufacturerItemDescription', 
-                'ManufacturerName', 
-                'ManufacturerID', 
-                'ManufacturerGLN', 
-                'QuantityOnHand', 
-                'InventoryUOM', 
-                'QuantityOnHandinCases', 
-                'QuantityOnHandinPounds', 
-                'QuantityShipped', 
-                'QuantityAvailable', 
-                'QuantityCommitted', 
-                'QuantityOnHold', 
-                'QuantityWIP', 
-                'UniqueRawIngredientValue', 
-                'QuantityNextDaySales', 
-                'QuantityonOrder', 
-                'QuantityReceived', 
-                'QuantityReturned', 
-                'QuantitySold', 
-                'QuantityWarehouseAdjustments', 
-                'ShelfLifeDaysRemaining', 
-                'NumberofPricingUOMperInventoryUOM', 
-                'NumberofInventoryUOMperCustomerInvoiceUOM', 
-                'LotNumber ', 
-                'ProductionDate',
-                'ExpirationDate', 
-                'S1ProductionReceiptQuantity', 
-                'S1ProductionReceiptDate', 
-                'S2ProductionReceiptQuantity', 
-                'S2ProductionReceiptDate', 
-                'S3ProductionReceiptQuantity', 
-                'S3ProductionReceiptDate']
-    cols_p = ['FileCreationDate', 
-              'FileCreationTime', 
-              'Brand', 
-              'LocationID',
-              'LocationName', 
-              'InventoryDate', 
-              'InventoryTime', 
-              'ItemName',
-              'ItemCasePackQuantity',
-              'ItemCasePackQuantityUOM',
-              'ManufacturerItemNumber', 
-              'ManufacturerItemDescription',
-              'ManufacturerName', 
-              'InventoryUOM', 
-              'LotNumber', 
-              'ProductionDate',
-              'ProductionQuantity', 
-              'FreezerInboundDate', 
-              'FreezerInboundQuantity']
-    
-    print(f'Writing inventory data of size (rows, cols) {df_i[cols_i].shape} to {desti_file_i}')
-    df_i[cols_i].to_csv(desti_file_i, sep=str('|'), index=False)
-    # df_i[cols_i].to_csv(desti_file_i, sep=str('|'), lineterminator= '\r\n', index=False)
-
-    print(f'Writing production data of size (rows, cols) {df_p[cols_p].shape} to {desti_file_p}')
-    df_p[cols_p].to_csv(desti_file_p, sep=str('|'), index=False)
-    
-    
-    return None
-
-def load() -> None:
-    return None
-
 if __name__ == "__main__":
     # file name format = supplier_name_suppinv_YYYYMMDD_HHmm
     #source_file = "AquaSource_Inventory_Production_Data.xlsx"
@@ -264,7 +160,5 @@ if __name__ == "__main__":
     desti_file_p = f"TEST_supplier_name_suppprod_{my_date}.txt"
 
     df_i, df_p = extract_gdrive(url, sheets)
-    #df_i, df_p = extract_local(data_dir, source_file, sheets)
     
-    #transf_write_local(df_i, df_p, desti_file_i, desti_file_p)
-    connect_load(df_i, df_p, desti_file_i, desti_file_p)
+    trans_conn_load(df_i, df_p, desti_file_i, desti_file_p)
